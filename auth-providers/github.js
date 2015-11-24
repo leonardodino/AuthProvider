a906a8c4fdd1b95d365530a811ff6eeb90bb949a
Leonardo Dino <leonardodino@users.noreply.github.com>
2015-11-24 16:28:32 -0200

    remove conflicting redirectUrl

--- auth-providers/github.js
+++ auth-providers/github.js
@@ -4,8 +4,6 @@ module.exports = {
   name:"github",
   authLocation: function(client_id,state,redirect_uri){
     return "https://github.com/login/oauth/authorize" +
-      "?client_id="+encodeURIComponent(client_id) +
-      "&state="+encodeURIComponent(state)+
-      "&redirect_uri="+encodeURIComponent(redirect_uri);
+      "?client_id="+encodeURIComponent(client_id);
   }
 };
